73fd7c309df01de8b24327a1c200efd7ff50e5e9
vlorian <vlorian.dervishi@gmail.com>
2023-07-31 13:09:33 -0400

    dirname to public folder

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3500;
 
+app.use("/",express.static(path.join(__dirname,'/public');
 app.listen(PORT, () => {
-  console.log(`listening on port${PORT}`);
+  console.log(`listening on port ${PORT}`);
 });
