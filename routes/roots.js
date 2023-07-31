ad42ad4db3f6d92c682ae4f3e0d6b8bc78160773
vlorian <vlorian.dervishi@gmail.com>
2023-07-31 13:24:12 -0400

    touched index.html

--- routes/roots.js
+++ routes/roots.js
@@ -6,3 +6,5 @@ const path = require("path");
 router.get("^/$|/index(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "views", "index.html"));
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3500;
 const path = require("path");
+const roots = require("./routes/roots");
 
 app.use("/", express.static(path.join(__dirname, "/public")));
-app.use("/", require("./routes/root"));
+app.use("/", roots);
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
