c4327a05b41093f4e25836b8e7b6befe6d85b4f6
vlorian <vlorian.dervishi@gmail.com>
2023-07-31 13:15:40 -0400

    )

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3500;
+const path = require("path");
 
-app.use("/",express.static(path.join(__dirname,'/public');
+app.use("/", express.static(path.join(__dirname, "/public")));
+app.use("/", require("./routes/root"));
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
